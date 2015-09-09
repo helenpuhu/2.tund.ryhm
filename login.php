d884fea485df171b3e24de26d15ae33da9646b23
Helen Puhu <helepuh@greeny.cs.tlu.ee>
2015-09-09 13:25:48 +0300

    updated login

--- login.php
+++ login.php
@@ -1,7 +1,28 @@
 <?php
 
 	//LOGIN.PHP
+	//echo $_POST["email"];
 	
+	//kontrollime et keegi vajutas input nuppu
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		//echo "keegi vajutas nuppu";
+		//konrollin, et epost poleks tühi
+		$email_error = "";
+		$password_error = "";
+		if ( empty($_POST["email"]) ) {
+			$email_error = "See väli on kohustuslik";
+		}
+		//kontrolln, et parool ei oleks tühi
+		
+		if ( empty($_POST["password"]) ) {
+			$password_error = "Parool on kohustuslik";
+		} else {
+		
+			if(strlen($_POST["password"]) < 8) {
+				$password_error = "Peab olema vähemalt 8 tähemärki pikk!";
+			}
+	}
+	}
 ?>
 
 <html>
@@ -10,10 +31,13 @@
 </head>
 <body>
 	<h2>Log in</h2>
-	
-		<input type="email" placeholder="E-post"> <br> <br>
-		<input type="password" placeholder="Parool"> <br> <br>
+		<form action="login.php" method="post">
+		<input name="email" type="email" placeholder="E-post"> <?php echo $email_error ?> <br> <br>
+		<input name="password" type="password" placeholder="Parool"> <?php echo $password_error ?> <br> <br>
 		<input type="submit" value="Log in"> <br> <br>
+		</form>
 	<h2>Create user </h2>
+	
+	
 </body>
 </html>
