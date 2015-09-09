6d01a1821069578ae5ef7e528b6a19fc0f49c220
Helen Puhu <helepuh@greeny.cs.tlu.ee>
2015-09-09 12:14:48 +0300

    For loop added

--- helloworld.php
+++ helloworld.php
@@ -2,4 +2,28 @@
 	$first_name="Helen";
 	$last_name="Puhu";
 	echo $first_name." ".$last_name;
+?>	
+<br>
+<?php
+	$age = 3;
+	//testib loogikat, juhul kui vanus on väiksem
+	//kui 18,siis kirjuta "alaealine", muul juhul "täisealine"
+	//ifi sisse loogikatehe
+	if($age < 18) {
+		echo "alaealine";
+		
+	} else {
+		echo "täisealine";
+	}
+?>	
+<br>
+<?php
+	//vastavalt vanusele trükime niimitu korda välja sõna "palju", kui on vanust	
+	
+	for($i = 0; $i < $age; $i = $i + 1) {
+		//tegevus mis kordub
+		echo "palju";
+	}
+	
+	echo "õnne!"
 ?>
